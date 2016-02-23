d907c4cde03c89e488fa63628c3b4b9ee1ceb5be
Cresse <ijcresse@gmail.com>
2016-02-22 21:21:54 -0800

    changed forest boss sprites, added giant boss ones

--- src/main.js
+++ src/main.js
@@ -63,11 +63,11 @@ AM.queueDownload("./img/forest-stage/forest trees.png");
 AM.queueDownload("./img/forest-stage/forest block.png");
 AM.queueDownload("./img/enemy/chaser.png");
 AM.queueDownload("./img/enemy/archer.png");
-AM.queueDownload("./img/enemy/forest boss spike 50px.png");
-AM.queueDownload("./img/enemy/forest boss spike 100px.png");
-AM.queueDownload("./img/enemy/forest boss spike 150px.png");
-AM.queueDownload("./img/enemy/forest boss platform.png");
-AM.queueDownload("./img/enemy/forest boss weak point.png");
+AM.queueDownload("./img/enemy/boss/forest boss spike 50px.png");
+AM.queueDownload("./img/enemy/boss/forest boss spike 100px.png");
+AM.queueDownload("./img/enemy/boss/forest boss spike 150px.png");
+AM.queueDownload("./img/enemy/boss/forest boss platform.png");
+AM.queueDownload("./img/enemy/boss/forest boss weak point.png");
 
 AM.queueStageDownload("./txt/forest-stage.txt");
 
